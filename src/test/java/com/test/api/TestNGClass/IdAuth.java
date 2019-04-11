49a42471ad783e4ea301eb86ded7e1a7e35fedca
chenxiaoxia <471256567@qq.com>
2019-04-11 17:30:12 +0800

    运行

---
+++ src/test/java/com/test/api/TestNGClass/IdAuth.java
@@ -0,0 +1,20 @@
+package com.test.api.TestNGClass;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class IdAuth {
+  @Test
+  public void f() {
+	  System.out.println("iiiiiii");
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
